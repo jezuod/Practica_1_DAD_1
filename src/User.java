3aeaed7a2fe505d8d840011b4a634e1cda6fc501
Jesus Ortin <jesusortindiaz@gmail.com>
2022-11-17 11:22:13 +0100

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,29 @@
+public class User {
+    //SISTEMA DE USUARIO
+    private String user_id; // ID USUARIO
+    private String user_password; // CONTRASEÑA
+    private Boolean admin; //ADMIN O NO
+
+    public String getUser_id() {
+        return user_id;
+    }
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public User(String user_id, String user_password) {
+        this.user_id = user_id;
+        this.user_password = user_password;
+        this.admin=false;
+    }
+
+    public User(String user_id, String user_password, Boolean admin) {
+        this.user_id = user_id;
+        this.user_password = user_password;
+        this.admin = admin;
+    }
+}
